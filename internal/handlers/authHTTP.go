665760f8afd9263f2b13da4c39fb69910096241b
lavinas <lavinas@me.com>
2022-10-08 13:47:10 -0300

    Formating

--- internal/handlers/authHTTP.go
+++ internal/handlers/authHTTP.go
@@ -2,29 +2,29 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/lavinas/payly-service/internal/core/ports"
 	"github.com/lavinas/payly-service/internal/core/domains"
+	"github.com/lavinas/payly-service/internal/core/ports"
 )
 
 type authHTTP struct {
 	authService ports.AuthService
 }
 
-func NewauthHTTP (authService ports.AuthService) *authHTTP {
+func NewauthHTTP(authService ports.AuthService) *authHTTP {
 	return &authHTTP{authService: authService}
 }
 
 func mountError(code int, message string, description string) domains.AuthError {
-	return domains.AuthError {
-		Code: code,
-		Error: message,
+	return domains.AuthError{
+		Code:        code,
+		Error:       message,
 		Description: description,
 	}
 }
 
-func throwError(c *gin.Context, err domains.AuthError){
+func throwError(c *gin.Context, err domains.AuthError) {
 	c.IndentedJSON(err.Code, err)
-} 
+}
 
 func (a *authHTTP) Token(c *gin.Context) {
 	var auth domains.AuthIn
